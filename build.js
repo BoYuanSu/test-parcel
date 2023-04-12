a9383a1916b0a436e9f3b0c82b3ce0a1c2c91dff
Paul Su <paul.su@baifu-tech.net>
2023-04-13 00:37:57 +0800

    backup dev

--- build.js
+++ build.js
@@ -1,13 +1,50 @@
+const Path = require('path')
 const { Parcel } = require('@parcel/core')
+const Bundler = require('parcel-bundler')
 
-const bundler = new Parcel({
-  entries: './demo/index.html',
-  defaultConfig: '@parcel/config-default',
-  shouldContentHash: false,
-  shouldDisableCache: true,
-  defaultTargetOptions: {
+function buildV2() {
+  const bundler = new Parcel({
+    entries: './demo/index.html',
+    defaultConfig: '@parcel/config-default',
+    shouldContentHash: false,
+    shouldDisableCache: true,
+    defaultTargetOptions: {
+      sourceMaps: false,
+      publicUrl: 'aaa',
+    },
+
+  })
+
+  bundler.run()
+}
+
+async function buildV1() {
+  const entryFiles = Path.join(__dirname, './demo/index.html')
+
+  const bundler = new Bundler(entryFiles, {
+    cache: false,
+    contentHash: false,
+    publicUrl: 'aaa',
     sourceMaps: false,
-  },
-})
+    minify: false,
+  })
+  bundler.on('bundled', (bundle) => {
+    console.log('🚀 ~ bundler.on ~ bundle:', bundle.entryAsset)
+    bundle.entryAsset?.type === 'html'
+      && bundle.entryAsset?.ast?.walk((node) => {
+        if (
+          node.type === 'tag'
+          && node.name === 'meta'
+        ) {
+          console.log('🚀 ~ &&bundle.entryAsset?.ast?.walk ~ node:', node)
+          const value = node.attribs.content
+          // Do something with the custom meta tag value, e.g. log it
+          console.log(`Custom meta value: ${value}`)
+        }
+      })
+  })
 
-bundler.run()
+  bundler.bundle()
+}
+
+buildV2()
---
+++ parcel-transformer-meta/index.js
@@ -0,0 +1,59 @@
+const { Transformer } = require('@parcel/plugin')
+
+function getMetaTag(html, property) {
+  const regex = new RegExp(`<meta[^>]*itemprop=["|']${property}["|'][^>]*>`, 'i')
+  const results = regex.exec(html)
+
+  if (!results)
+    throw new Error(`Missing ${property}`)
+
+  return results[0]
+}
+
+function getMetaTagContent(metaTagHtml) {
+  const contentRegex = /content="([^"]*)"/i
+  const results = contentRegex.exec(metaTagHtml)
+
+  if (!results)
+    throw new Error(`Missing content attribute in ${chalk.bold(metaTagHtml)}`)
+
+  return results[1]
+}
+
+module.exports = new Transformer({
+  async transform({ asset }) {
+    const html = await asset.getCode()
+
+    try {
+      const metaImageTag = getMetaTag(html, 'image')
+      console.log('🚀 ~ transform ~ ogImageTag:', ogImageTag)
+      const ogImageContent = getMetaTagContent(ogImageTag)
+
+      const twitterImageTag = getMetaTag(html, 'twitter:image')
+      const twitterImageContent = getMetaTagContent(twitterImageTag)
+
+      const ogUrlTag = getMetaTag(html, 'og:url')
+      const ogUrlContent = getMetaTagContent(ogUrlTag)
+
+      // instead of url.resolve, we just append directly.
+      // the reason is it seems like Parcel 2 just goes through
+      // and replaces content hash with string replace and adds a `/`
+      // in front of it. So if we do url resolve then we get a double
+      // `//` and the link won't work.
+      const absoluteImageUrl = ogUrlContent + ogImageContent
+
+      const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteImageUrl)
+      const twitterImageTagAbsoluteUrl = twitterImageTag.replace(twitterImageContent, absoluteImageUrl)
+
+      // we assume the twitter:image meta tag has the same URL as the og:image tag
+      const patchedHtml = html.replace(ogImageTag, ogImageTagAbsoluteUrl).replace(twitterImageTag, twitterImageTagAbsoluteUrl)
+
+      asset.setCode(patchedHtml)
+    }
+    catch (error) {
+      console.log(error.message)
+    }
+
+    return [asset]
+  },
+})
